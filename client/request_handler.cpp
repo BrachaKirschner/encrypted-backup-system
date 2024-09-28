7df465028eec0005c20d78dd19ff6004c8d58645
Bracha Kirschner <bracha10000@gmail.com>
2024-09-28 22:45:40 +0300

    "commit"

---
+++ client/request_handler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brach on 25/09/2024.
+//
+
+#include "request_handler.h"
---
+++ client/request_handler.h
@@ -0,0 +1,16 @@
+//
+// Created by brach on 25/09/2024.
+//
+
+#ifndef REQUEST_HANDLER_H
+#define REQUEST_HANDLER_H
+
+
+
+class request_handler {
+
+};
+
+
+
+#endif //REQUEST_HANDLER_H
